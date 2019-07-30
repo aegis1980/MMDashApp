b0baa13ad6b0d9a5be57b71f76496f7037381f50
Jon Robinson <jonrobinson@hotmail.com>
2019-07-30 14:29:25 +1200

    assets fix

--- Application/python_webapp_flask/__init__.py
+++ Application/python_webapp_flask/__init__.py
@@ -10,7 +10,7 @@ ASSETS_PATH = os.path.join(os.getcwd(),'python_webapp_flask','assets')
 # LUX is a bootswatch theme which is included in dash-bootstrap
 # additional styling in assets/styling.css (which overides bootstrap theme) 
 
-dash_app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+dash_app = dash.Dash(__name__,external_stylesheets=[dbc.themes.LUX])
 dash_app.config.suppress_callback_exceptions = True
 dash_app.enable_dev_tools(debug=True)
 
